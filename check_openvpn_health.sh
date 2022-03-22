595bc8f5a754d3f7b4ec452876273043696bc6a4
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2022-03-22 13:30:13 +0300

    renamed restart_openvpn_client.sh

--- check_openvpn_health.sh
+++ check_openvpn_health.sh
@@ -9,7 +9,7 @@ function log {
 log "Checking route to google.com..."
 if ! nc -z -w 1 www.google.com 80 > /dev/null 2>&1 ; then
 	log "🔥 No route to google.com 🔥"
-	/home/user/restart_openvpn.sh
+	./restart_openvpn_client.sh
 else
 	log "Checking ip..."
 	IP=$(curl -Ls https://api.myip.com)
@@ -18,7 +18,7 @@ else
 		log "✅ All good! ✅"
 	else
 		log "🔥 Bad IP! 🔥"
-		/home/user/restart_openvpn.sh
+		./restart_openvpn_client.sh
 	fi
 fi
 
