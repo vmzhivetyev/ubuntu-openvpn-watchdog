ba12db9cb9aceb0d64ecceeedb16a3fc2daef193
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2022-03-22 14:17:32 +0300

    added timer

--- install_service.sh
+++ install_service.sh
@@ -5,29 +5,46 @@ set -euo pipefail
 set -x
 
 SERVICE_NAME=vpn_health_checker
-INSTALL_PATH=/lib/systemd/system/$SERVICE_NAME.service
+INSTALL_PATH=/lib/systemd/system
 SERVICE_FILE=$(pwd)/$SERVICE_NAME.service
+TIMER_FILE=$(pwd)/$SERVICE_NAME.timer
 
 if [ ! -f $SERVICE_FILE ]; then
 	echo "Error: $SERVICE_FILE doesn't exist"
 	exit 1
 fi
 
+if [ ! -f $TIMER_FILE ]; then
+	echo "Error: $TIMER_FILE doesn't exist"
+	exit 1
+fi
+
 # Stop service if running
 systemctl stop $SERVICE_NAME || true
 systemctl disable $SERVICE_NAME || true
+systemctl stop $SERVICE_NAME.timer || true
+systemctl disable $SERVICE_NAME.timer || true
 
 # Verify service file
 systemd-analyze verify $SERVICE_FILE
+systemd-analyze verify $TIMER_FILE
 
 # Install service files
-if [ -f $INSTALL_PATH ]; then
-	rm $INSTALL_PATH
+if [ -f $INSTALL_PATH/$SERVICE_NAME.service ]; then
+	rm $INSTALL_PATH/$SERVICE_NAME.service
 fi
-ln -s $SERVICE_FILE $INSTALL_PATH
+ln -s $SERVICE_FILE $INSTALL_PATH/$SERVICE_NAME.service
 
-# Start service
-systemctl enable $SERVICE_NAME
-systemctl start $SERVICE_NAME
+if [ -f $INSTALL_PATH/$SERVICE_NAME.timer ]; then
+	rm $INSTALL_PATH/$SERVICE_NAME.timer
+fi
+ln -s $TIMER_FILE $INSTALL_PATH/$SERVICE_NAME.timer
+
+# Start service timer
+# systemctl enable $SERVICE_NAME
+systemctl enable $SERVICE_NAME.timer
+
+echo "Run: systemctl start $SERVICE_NAME"
+echo "Run: ./watch_log.sh"
 
 
