6239bd5aa3f3a503254ff918b94edaf2ff4064ff
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2022-03-25 12:33:20 +0300

    Recheck IP after restart

--- check_openvpn_health.sh
+++ check_openvpn_health.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
+echo ''
+echo ''
+
 set -euo pipefail
 
 cd /home/user/ubuntu-openvpn-watchdog
 
-echo "Checking route to google.com..."
+MAX_ATTEMPTS=3
+RECHECK_DELAY=20 # seconds before first recheck in a row
+
 if ! nc -z -w 1 www.google.com 80 > /dev/null 2>&1 ; then
 	echo "🔥 No route to google.com 🔥"
 	./restart_openvpn_client.sh
 else
-	echo "Checking ip..."
-	IP=$(curl -Ls https://api.myip.com)
-	echo "Current IP: $IP"
-	if echo $IP | grep -qiF Germany ; then
-		echo "✅ All good! ✅"
-	else
-		echo "🔥 Bad IP! 🔥"
-		./restart_openvpn_client.sh
-	fi
+	ATTEMPT=1
+	
+	while [[ $ATTEMPT -le $MAX_ATTEMPTS ]]; do
+		IP=$(curl -Ls https://api.myip.com)
+		if echo $IP | grep -qiF Germany ; then
+			echo "✅ All good! ✅"
+			exit 0
+		else
+			echo "🔥 Bad IP! $IP 🔥"
+			echo "Restart attempt $ATTEMPT/$MAX_ATTEMPTS..."
+			./restart_openvpn_client.sh
+
+			ATTEMPT=$(( $ATTEMPT + 1 ))
+			DELAY=$(( $RECHECK_DELAY * ($ATTEMPT - 1) )) # wait 20, 40, 60 for rechecks in a row
+			echo "Waiting $DELAY seconds before recheck..."
+			sleep $DELAY
+		fi
+	done
+
+	echo "Reached retries limit. Exiting."
 fi
 
-echo
+echo ''
+echo ''
